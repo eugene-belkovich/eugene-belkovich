f7cec6784950439305de1c22f7785523cc38c6dc
jeka <eugene.belkovich@gmail.com>
2016-12-07 15:31:44 +0300

    replace carousel, add new images, change ui

--- js/accordion.js
+++ js/accordion.js
@@ -1,9 +1,27 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        margin:10,
+        loop:true,
+        autoWidth:true,
+        smartSpeed: 300,
+        fluidSpeed: 300,
+        autoplaySpeed: 300,
+        nav: true,
+        lazyLoad: true
+    });
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
+    let acc = document.getElementsByClassName("accordion");
+    let i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function () {
+            if (!this.classList.contains("active")) {
+                $('html, body').animate({
+                    scrollTop: $(this).offset().top - 50
+                }, 500);
+            }
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
+        }
     }
-}
+});
